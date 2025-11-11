cd80a5b94541803894049d611475d2caf8fe99c4
Alejandro Adriel <alejandroadrielq@gmail.com>
2025-11-11 00:50:55 -0500

    Restrictions were added to the recipes

--- src/models/recipeModel.js
+++ src/models/recipeModel.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
+const ALLOWED_RESTRICTIONS = ["vegetariano", "sinlacteos", "singluten"];
+
 const recipeSchema = new mongoose.Schema(
   {
     _id: {
       type: String,
-      required: true
+      required: true,
     },
     name: {
       type: String,
@@ -16,7 +18,7 @@ const recipeSchema = new mongoose.Schema(
     },
     image: {
       type: String,
-      required: true
+      required: true,
     },
     cookTime: {
       type: Number,
@@ -35,34 +37,45 @@ const recipeSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    restricciones: {
+      type: [String],
+      default: [],
+      enum: {
+        values: ALLOWED_RESTRICTIONS,
+        message: `Restricción no válida. Las opciones permitidas son: ${ALLOWED_RESTRICTIONS.join(
+          ", "
+        )}.`,
+      },
+    },
     ingredients: [String],
-    instructions: [String]
+    instructions: [String],
   },
-  { 
+  {
     timestamps: true,
-    _id: false 
+    _id: false,
   }
 );
 
-recipeSchema.pre('validate', async function(next) {
+recipeSchema.pre("validate", async function (next) {
   if (this.isNew && !this._id) {
     try {
-      const lastRecipe = await mongoose.model('Recipe')
+      const lastRecipe = await mongoose
+        .model("Recipe")
         .findOne()
         .sort({ _id: -1 });
-      
+
       let nextNumber = 1;
       if (lastRecipe && lastRecipe._id) {
         nextNumber = parseInt(lastRecipe._id) + 1;
       }
-      
-      const recipeId = String(nextNumber).padStart(6, '0');
+
+      const recipeId = String(nextNumber).padStart(6, "0");
       this._id = recipeId;
       this.image = `/assets/images/recipes/${recipeId}.jpg`;
-      
+
       next();
     } catch (error) {
-      console.error('Error generando ID:', error);
+      console.error("Error generando ID:", error);
       next(error);
     }
   } else {
