fbef34e164c339b3a7df9f26e21dca8eca18afaa
Alejandro Adriel <alejandroadrielq@gmail.com>
2025-11-11 02:57:26 -0500

    Fix: Add missing await in mongoose connection and ensure consistent semicolon usage

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,12 @@
-import mongoose, { trusted } from "mongoose"
+import mongoose, { trusted } from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        const dbURI = process.env.MONGODB_URI
-
-        mongoose.connect(dbURI)
-        console.log("Conectado a MongoDB correctamente")
-    } catch (error) {
-        console.error("Error al conectar con MongoDB", error)
-        process.exit(1)
-    }
-}
+  try {
+    const dbURI = process.env.MONGODB_URI;
+    await mongoose.connect(dbURI);
+    console.log("Conectado a MongoDB correctamente");
+  } catch (error) {
+    console.error("Error al conectar con MongoDB", error);
+    process.exit(1);
+  }
+};
